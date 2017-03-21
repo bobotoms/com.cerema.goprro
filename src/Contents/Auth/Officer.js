f0968fe5ffdde31ad6f43739f2c6dbcd48122a19
bobotoms <bobotoms@yahoo.fr>
2017-03-21 16:45:37 +0100

    Update# 60

--- src/Contents/Auth/Officer.js
+++ src/Contents/Auth/Officer.js
@@ -31,20 +31,16 @@ Officer = {
 			
 			profile=profile.username;
             */
-            console.log("profile");
-            console.log(profile);
+            
 			var mail=profile.username.email;
-            console.log("mail");
-            console.log(profile.email);
 			Officer.using('db').store('bpclight','select kage,nom,prenom from agents where kage in (select kage from mela where libmela="'+mail+'")',function(err,result){
 				if (!err) {
                     var response={
-						/*lastname: result.data[0].nom,
-						firstname: result.data[0].prenom,*/
-						//uid: result.data[0].kage,
-                        //result:result,
+						lastname: result.data[0].nom,
+						firstname: result.data[0].prenom,
+						uid: result.data[0].kage,
 						mail: mail,
-						//profiles: Officer.getProfile(mail.split('@')[0])
+						profiles: Officer.getProfile(mail.split('@')[0])
 					};
 					cb(response);			
 				} else cb(err);
