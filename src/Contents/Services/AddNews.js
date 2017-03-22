238b0f3265901c78230b3a5ea83ecd6a37f742f8
bobotoms <bobotoms@yahoo.fr>
2017-03-22 09:28:54 +0100

    Update# 68

--- src/Contents/Services/AddNews.js
+++ src/Contents/Services/AddNews.js
@@ -1,7 +1,8 @@
 AddNews = {
 	insert: function(o,cb) {
 		//Elements.using('db').query("goprro",o,cb);	
-        
+        console.log("o");
+        console.log(o);
 		Elements.using('db').query("goprro","INSERT INTO notes (dateNote, texteNote, idUser) VALUES (NOW(), '"+o+"', 1)",cb);
         
 	}
