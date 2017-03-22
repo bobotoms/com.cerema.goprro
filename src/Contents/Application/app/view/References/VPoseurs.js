f634d5dde3c3ab83eadcad0f34a462211a166d05
bobotoms <bobotoms@yahoo.fr>
2017-03-22 14:54:53 +0100

    Update# 114

--- src/Contents/Application/app/view/References/VPoseurs.js
+++ src/Contents/Application/app/view/References/VPoseurs.js
@@ -33,7 +33,7 @@ App.view.define('References.VPoseurs', {
                     plugins: [
                     {
                         ptype: "cellediting",
-                        clicksToEdit: 1
+                        clicksToEdit: 2
                     }
                     ],
                     columns: [
