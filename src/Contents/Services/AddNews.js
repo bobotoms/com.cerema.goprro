8a97821f178328788496e30537f5f837e239383e
bobotoms <bobotoms@yahoo.fr>
2017-03-22 10:02:07 +0100

    Update# 85

--- src/Contents/Services/AddNews.js
+++ src/Contents/Services/AddNews.js
@@ -4,8 +4,7 @@ AddNews = {
         var mail = o['0'];
         var note = o['1'];
                 
-		//AddNews.using('db').query("goprro","INSERT INTO notes (dateNote, texteNote, idUser) VALUES (NOW(), '"+note+"', (select idUser from users where mail='"+mail+"'))",cb);
-		AddNews.using('db').query("goprro","INSERT INTO notves (dateNote, texteNote, idUser) VALUES (NOW(), '"+note+"', (select idUser from users where mail='"+mail+"'))",function(err,result){
+		AddNews.using('db').query("goprro","INSERT INTO notes (dateNote, texteNote, idUser) VALUES (NOW(), '"+note+"', (select idUser from users where mail='"+mail+"'))",function(err,result){
 				if (!err) {
                     var response=true;
 					cb(response);			
