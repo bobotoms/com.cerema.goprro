5d60cf1ce994375f4f64c51118e8088b6e506332
bobotoms <bobotoms@yahoo.fr>
2017-03-22 10:01:34 +0100

    Update# 84

--- src/Contents/Services/AddNews.js
+++ src/Contents/Services/AddNews.js
@@ -5,7 +5,7 @@ AddNews = {
         var note = o['1'];
                 
 		//AddNews.using('db').query("goprro","INSERT INTO notes (dateNote, texteNote, idUser) VALUES (NOW(), '"+note+"', (select idUser from users where mail='"+mail+"'))",cb);
-		AddNews.using('db').query("goprro","INSERT INTO notes (dateNote, texteNote, idUser) VALUES (NOW(), '"+note+"', (select idUser from users where mail='"+mail+"'))",function(err,result){
+		AddNews.using('db').query("goprro","INSERT INTO notves (dateNote, texteNote, idUser) VALUES (NOW(), '"+note+"', (select idUser from users where mail='"+mail+"'))",function(err,result){
 				if (!err) {
                     var response=true;
 					cb(response);			
@@ -19,18 +19,3 @@ AddNews = {
 }
 
 module.exports = AddNews;
-
-/*
-
-Officer.using('db').store('bpclight','select kage,nom,prenom from agents where kage in (select kage from mela where libmela="'+mail+'")',function(err,result){
-				if (!err) {
-					var response={
-						lastname: result.data[0].nom,
-						firstname: result.data[0].prenom,
-						uid: result.data[0].kage,
-						mail: mail,
-						profiles: Officer.getProfile(mail.split('@')[0])
-					};
-					cb(response);			
-				} else cb(err);
-			});*/
