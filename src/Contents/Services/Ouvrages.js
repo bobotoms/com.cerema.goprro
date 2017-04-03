60cd8e6d912f27dc67b1c50fcb2a6202a5031f29
bobotoms <bobotoms@yahoo.fr>
2017-04-03 09:39:55 +0200

    Update# 168

--- src/Contents/Services/Ouvrages.js
+++ src/Contents/Services/Ouvrages.js
@@ -5,8 +5,9 @@ Ouvrages = {
         db.model("goprro",db.sql("OAGetAll"),cb);
     },
     select: function(o,cb) {
-        /*
+        
         console.log("user mail");
+        /*var mail=profile.username.email;
         var mail = Auth.User.mail;
         console.log(mail);
         */
