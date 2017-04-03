abdffdcb9b8c0492bc7d63fa00987070ae5eabd2
bobotoms <bobotoms@yahoo.fr>
2017-04-03 10:07:51 +0200

    Update# 178

---
+++ src/Contents/Services/Visits.js
@@ -0,0 +1,27 @@
+Visits = {
+    getAll: function(o,cb) {
+        var db=Ouvrages.using('db');
+        db.model("goprro",db.sql("OAGetAll"),cb);
+    },/*
+    select: function(o,cb) {*/
+        /*var mail=profile.username.email;
+        var mail = Auth.User.mail;
+        console.log(mail);
+        */
+ /*		var db=Ouvrages.using('db');
+		db.model("goprro",db.sql("OAGetAll"),function(error,response){
+            console.log('******* USER MAIL *****');
+            console.log(response);
+            response.metaData.fields.push({
+                name: "select",
+                type: "boolean"
+            });
+            for (var i=0;i<response.data.length;i++) {
+                response.data[i].select="0";
+            }
+            cb(response);
+        });       
+    }*/
+};
+
+module.exports = Visits;
