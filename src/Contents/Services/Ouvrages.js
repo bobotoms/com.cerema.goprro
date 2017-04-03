bb94d87666fd8bb06e21b26550b49b70d1ae2a2e
bobotoms <bobotoms@yahoo.fr>
2017-04-03 09:25:06 +0200

    Update# 164

--- src/Contents/Services/Ouvrages.js
+++ src/Contents/Services/Ouvrages.js
@@ -3,6 +3,20 @@ Ouvrages = {
     getAll: function(o,cb) {
         var db=Ouvrages.using('db');
         db.model("goprro",db.sql("OAGetAll"),cb);
+    },
+    select: function(o,cb) {
+ 		var db=Ouvrages.using('db');
+		db.model("goprro",db.sql("OAGetAll"),function(error,response){
+            console.log(response);
+            response.metaData.fields.push({
+                name: "select",
+                type: "boolean"
+            });
+            for (var i=0;i<response.data.length;i++) {
+                response.data[i].select="0";
+            }
+            cb(response);
+        });       
     }
 };
 
