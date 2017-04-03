97cf5e505375043fcbe5c826cd700b88c7262f30
bobotoms <bobotoms@yahoo.fr>
2017-04-03 13:23:29 +0200

    Update# 232

--- src/Contents/Services/Visits.js
+++ src/Contents/Services/Visits.js
@@ -13,6 +13,20 @@ Visits = {
 				};
         });       
     },
+    selectVisit: function(o,cb) {
+ 		Visits.using('db').query("goprro","SELECT * FROM visite_ouvrages left join familles on ouvrages.idFamille=familles.idFamille left join types on types.idType=ouvrages.idType left join geologies on geologies.idGeologie=ouvrages.idGeologie left join situations on situations.idSituation=ouvrages.idSituation left join acces on acces.idAcces=ouvrages.idAcces left join departements on departements.idDepartement=ouvrages.idDepartement WHERE departements.idDter = (SELECT idDter FROM users WHERE mail='"+o+"')",function(err,result){
+            if (!err) {
+                    console.log("result");
+                    console.log(result);
+					cb(result);			
+				} else {
+                    console.log("err");
+                    console.log(err);
+                    var err=false;
+					cb(err)			
+				};
+        });       
+    }
  /*		var db=Ouvrages.using('db');
 		db.model("goprro",db.sql("OAGetAll"),function(error,response){
             console.log('******* USER MAIL *****');
