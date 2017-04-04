d3eaaf333ab40541c7be31bdad6820a3d54147a5
bobotoms <bobotoms@yahoo.fr>
2017-04-04 10:19:30 +0200

    Update# 329

--- src/Contents/Application/app/controller/CMain.js
+++ src/Contents/Application/app/controller/CMain.js
@@ -1,7 +1,8 @@
 var TMap={};
 
 function hideForms() {
-    var form=App.get('mainform panel#CPanel');
+    //var form=App.get('mainform panel#CPanel');
+    var form=App.get('VAddVisit panel#addVisitMap');
     for (var i=0;i<form.items.length;i++) form.items.items[i].hide();
 };
 
