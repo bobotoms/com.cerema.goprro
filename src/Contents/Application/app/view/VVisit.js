59b6ffdc847214fe166d34eeea10525181747cbb
bobotoms <bobotoms@yahoo.fr>
2017-04-04 14:54:38 +0200

    Update# 422

--- src/Contents/Application/app/view/VVisit.js
+++ src/Contents/Application/app/view/VVisit.js
@@ -84,7 +84,7 @@ App.view.define('VVisit', {
                   click: function() {
                      // var test  = Ext.getCmp('#idVisiteOuvrage').getValue();
                       //var test  = Ext.getCmp('VVisit grid#idVisiteOuvrage');
-                      var test  = this.get('VVisit grid#idVisiteOuvrage').getValue();
+                      var test  = App.get('VVisit grid#idVisiteOuvrage').value();
                       console.log("test");
                       console.log(test);
                      Ext.MessageBox.alert('Alert box', 'Button 1 is clicked'+test);	
