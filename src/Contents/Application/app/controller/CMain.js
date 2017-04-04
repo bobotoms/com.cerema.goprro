4154e197c9c17393a7d64d1d31a823f560282aeb
bobotoms <bobotoms@yahoo.fr>
2017-04-04 12:04:20 +0200

    Update# 390

--- src/Contents/Application/app/controller/CMain.js
+++ src/Contents/Application/app/controller/CMain.js
@@ -176,7 +176,7 @@ App.controller.define('CMain', {
                 show: "VSaisie_onShow"
             },
             "VVisit actioncolumn#delOuvrageVisit": {
-                click: "dell_ouvrage_visit"
+                itemdblclick: "dell_ouvrage_visit"
             },
         });
 
