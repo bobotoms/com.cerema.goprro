86a191e327a4aca7551e49aa81f8b2590d3b3ede
bobotoms <bobotoms@yahoo.fr>
2017-04-05 13:54:37 +0200

    Update# 533

--- src/Contents/Application/app/view/VVisit.js
+++ src/Contents/Application/app/view/VVisit.js
@@ -85,9 +85,12 @@ App.view.define('VVisit', {
                 listeners: {
                     click: function(sm,index,record) {
                         var idVisiteOuvrage = sm.store.data.items[record].data.idVisiteOuvrage;
+                        console.log("id viste ouvrage");
+                        console.log(idVisteOuvrage);
                         /*ici, tu mets tous tes traitements lors d'un clic sur une ligne*/
                         /*je t ai mis une alerte ... */
                         /*pour utiliser un parametre de ta ligne, tu utilises record.data.[nom de la colonne]*/
+                        
                     }
                 }
 
