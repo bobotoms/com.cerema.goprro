2df2a2289402a2dd76c39e6d40b07c28996687b7
bobotoms <bobotoms@yahoo.fr>
2017-04-05 10:51:30 +0200

    Update# 475

--- src/Contents/Application/app/controller/CMain.js
+++ src/Contents/Application/app/controller/CMain.js
@@ -25,8 +25,8 @@ function GMap(l,m)
         marker.setMap(TMap.map);
         marker.addListener('click', function(x) {
             hideForms();
-            //var form=App.get("mainform panel#Saisie");
-            var form=App.get("VAddVisit panel#Saisie");
+            var form=App.get("mainform panel#Saisie");
+            //var form=App.get("VAddVisit panel#Saisie");
             //var form=App.get("VAddVisit panel#gridVisitAdd");
             form.idOuvrage=this.itemId;
             form.show();
