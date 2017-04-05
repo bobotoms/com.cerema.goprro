408edfd58afdf179073faf92d5537e802a39a508
bobotoms <bobotoms@yahoo.fr>
2017-04-05 09:38:08 +0200

    Update# 461

--- src/Contents/Application/app/view/VAddV.js
+++ src/Contents/Application/app/view/VAddV.js
@@ -23,6 +23,34 @@ App.view.define('VAddV', {
                     width: "100%",
                     border: false,
                     split: true
+                },
+                {
+                    xtype: "grid",
+                    hidden: true,
+                    itemId: "gridO",
+                    columns: [
+                        {
+                            text: "Ouvrage",
+                            flex: 1,
+                            dataIndex: "nomOuvrage"
+                        },
+                        {
+                            text: "Dpt",
+                            width: 150,
+                            dataIndex: "nomDepartement"
+                        },
+                        {
+                            text: "Longitude",
+                            width: 150,
+                            dataIndex: "oa_y"
+                        },
+                        {
+                            text: "Latitude",
+                            width: 150,
+                            dataIndex: "oa_x"
+                        }
+                    ],
+                    store: App.store.create("App.Ouvrages.getAll",{autoLoad: true})
                 }
                 ],
     }]
