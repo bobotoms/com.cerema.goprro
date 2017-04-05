42a431543091187bb996dfe620014505380f8d59
bobotoms <bobotoms@yahoo.fr>
2017-04-05 11:06:10 +0200

    Update# 491

--- src/Contents/Application/app/controller/CMain.js
+++ src/Contents/Application/app/controller/CMain.js
@@ -596,9 +596,9 @@ App.controller.define('CMain', {
         console.log("showMapV");
         hideForms();
         TMap.clearMarkers();
-        App.get("VAddV panel#CPanel").show();
-        console.log("VAddV #mapV");
+        console.log("VAddV panel#mapV");
         console.log(App.get("VAddV panel#mapV"));
+        App.get("VAddV panel#CPanel").show();
         App.DB.get("goprro://ouvrages{idOuvrage,oa_x,oa_y,nomOuvrage,idOuvrage}",function(r) {
             for (var i=0;i<r.data.length;i++) {
                 TMap.setMarker(r.data[i].oa_y,r.data[i].oa_x,r.data[i].nomOuvrage,r.data[i].idOuvrage);
