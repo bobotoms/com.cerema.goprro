ca9639f06d58b9a6163d60a7f86ec1a175ae412d
bobotoms <bobotoms@yahoo.fr>
2017-04-05 10:59:16 +0200

    Update# 485

--- src/Contents/Application/app/view/VAddV.js
+++ src/Contents/Application/app/view/VAddV.js
@@ -8,69 +8,6 @@ App.view.define('VAddV', {
 
     items: [
         {
-            region: 'north',
-            height: 25,
-            minHeight: 25,
-            border:false,
-            baseCls: 'cls-header',
-            xtype: "Menu",
-            itemId: "MenuPanel",
-            menu: [
-                {
-                    text: "Général",
-                    menu: [
-                        {
-                            text: "Ouvrages",
-                            menu: [
-                                {
-                                    text: "Nouveau"
-                                },
-                                {
-                                    text: "Carte"
-                                },
-                                {
-                                    text: "Liste"
-                                }
-                            ]
-                        }
-                    ]
-                },
-                {
-                    text: "Visites"
-                }
-            ]
-        },
-        {
-            region: 'north',
-            xtype: "ribbon"
-        },
-        {
-            title: "Actualités"
-            , region: 'south'
-            , itemId: "southpanel"
-            , border: false
-            , collapsible: true
-            , height: 195
-            , layout: "vbox"
-            , tbar: [
-            {
-                itemId: "ecrire"
-                , text: "Ecrire"
-                , iconCls: "pencil"
-            }
-        ]
-            , items: [
-            {
-                width: "100%"
-                , itemId: "timeline"
-                , border: false
-                , autoScroll: true
-                , html: '<ul class="timeline"></ul>'
-                , flex: 1
-            }
-        ]
-        },
-        {
             region: "center",
             split:true,
             layout:"fit",
@@ -86,64 +23,6 @@ App.view.define('VAddV', {
                     width: "100%",
                     border: false,
                     split: true
-                },
-                {
-                    xtype: "grid",
-                    hidden: true,
-                    itemId: "gridO",
-                    columns: [
-                        {
-                            text: "Ouvrage",
-                            flex: 1,
-                            dataIndex: "nomOuvrage"
-                        },
-                        {
-                            text: "Dpt",
-                            width: 150,
-                            dataIndex: "nomDepartement"
-                        },
-                        {
-                            text: "Longitude",
-                            width: 150,
-                            dataIndex: "oa_y"
-                        },
-                        {
-                            text: "Latitude",
-                            width: 150,
-                            dataIndex: "oa_x"
-                        }
-                    ],
-                    store: App.store.create("App.Ouvrages.getAll",{autoLoad: true})
-                },
-                {
-                    xtype: "VCharacteristics",
-                    itemId: "setup_characteristics",
-                    hidden: true
-                },
-                {
-                    xtype: "VSaisie",
-                    itemId: "Saisie",
-                    hidden: true
-                },
-                {
-                    xtype: "VRefs",
-                    itemId: "setup_refs",
-                    hidden: true
-                },
-				{
-					xtype: "VAddVisit",
-					itemId: "addVisit",
-					hidden: true
-				},
-				{
-					xtype: "VVisit",
-					itemId: "visit",
-					hidden: true
-				},
-                {
-                    xtype: "VVisitWork",
-                    itemId: "Work",
-                    hidden: true
                 }
             ]
         }
