f5b14cd330e71226e7dca2a49ddccd700dd37419
bobotoms <bobotoms@yahoo.fr>
2017-04-05 13:53:27 +0200

    Update# 532

--- src/Contents/Application/app/view/VVisit.js
+++ src/Contents/Application/app/view/VVisit.js
@@ -78,87 +78,19 @@ App.view.define('VVisit', {
                 dataIndex: "oa_x"
             },
             {
- /*               xtype: 'checkcolumn',
-                text: "Sélectionner",
-                dataIndex: "select",
-                cls: 'x-grid-checkheader-editor',*/
-/*                renderer: function(value,a,b,c){
-
-                                var items=App.store.familles.data.items;
-                                var Items={};
-                                for (var i=0;i<items.length;i++) Items[items[i].data.idFamille]=items[i].data.nomFamille;
-                                return iconCls: "delgrid";
-
-                            },*/
                 xtype: 'actioncolumn',
                 itemId: "delOuvrageVisit", 
                 iconCls: "delgrid",
-     //           sm: new Ext.grid.RowSelectionModel({
-                  singleSelect: true,
-                  listeners: {
-                     click: function(sm,index,record) {
-                         var idVisiteOuvrage = sm.store.data.items[record].data.idVisiteOuvrage;
-                            /*ici, tu mets tous tes traitements lors d'un clic sur une ligne*/
-                            /*je t ai mis une alerte ... */
-                            /*pour utiliser un parametre de ta ligne, tu utilises record.data.[nom de la colonne]*/
-                             }
-                         }
-       //               }),
-    /*            listeners : {
-                    click: function() {
-                        var id0 = Ext.getCmp("idtest");
-                      console.log("id0");
-                      console.log(id0);*/
-    /*                    var id1 = Ext.getCmp("idtest").value;
-                      console.log("id1");
-                      console.log(id1);
-                        var id2 = Ext.getCmp("idtest").value();
-                      console.log("id2");
-                      console.log(id2);*/
-  /*                      var id3 = Ext.getCmp("idtest").val;
-                      console.log("id3");
-                      console.log(id3);
-                        var id4 = Ext.getCmp("idtest").val();
-                      console.log("id4");
-                      console.log(id4);*/
- /*                       var id5 = Ext.getCmp("idtest").getValue();
-                      console.log("id5");
-                      console.log(id5);*/
-       /*                 var id1 = Ext.getCmp(idtest);
-                      console.log("id1");
-                      console.log(id1);
-                        var id2 = Ext.getCmp("#idtest");
-                      console.log("id2");
-                      console.log(id2);
-                        var id3 = Ext.getCmp("#idVisiteOuvrage");
-                      console.log("id3");
-                      console.log(id3);
-                        var id4 = Ext.getCmp("idVisiteOuvrage");
-                      console.log("id4");
-                      console.log(id4);*/
-  /*            
-                        Ext.MessageBox.alert('Alert box', 'Button 1 is clicked');	
+                singleSelect: true,
+                listeners: {
+                    click: function(sm,index,record) {
+                        var idVisiteOuvrage = sm.store.data.items[record].data.idVisiteOuvrage;
+                        /*ici, tu mets tous tes traitements lors d'un clic sur une ligne*/
+                        /*je t ai mis une alerte ... */
+                        /*pour utiliser un parametre de ta ligne, tu utilises record.data.[nom de la colonne]*/
                     }
-                }*/
-                /*listeners: {
-                  click: function() {
-                     // var test  = Ext.getCmp('#idVisiteOuvrage').getValue();
-                      //var test  = Ext.getCmp('VVisit grid#idVisiteOuvrage');
-                      var test  = App.get('VVisit grid#idVisiteOuvrage').value();
-                      console.log("test");
-                      console.log(test);
-                     Ext.MessageBox.alert('Alert box', 'Button 1 is clicked'+test);	
-                  }
-                }*/
-               /* items: [
-                {
-                    iconCls: "delgrid",
-                    listeners: {
-                      click: function() {
-                         Ext.MessageBox.alert('Alert box', 'Button 1 is clicked');	
-                      }
-               }
-                }]*/
+                }
+
             },
             {
                 xtype: 'actioncolumn',
