908a0950e5e10fb13f7a0119b0d8941dfbc54d1e
bobotoms <bobotoms@yahoo.fr>
2017-04-06 13:56:10 +0200

    Update# 612

--- src/Contents/Application/app/controller/CMain.js
+++ src/Contents/Application/app/controller/CMain.js
@@ -988,12 +988,24 @@ App.controller.define('CMain', {
         });      
 
         
+         /*     
+       
+        TMap.clearMarkers();
+        App.get("VAddVisit panel#map").show();
+        App.DB.get("goprro://ouvrages{idOuvrage,oa_x,oa_y,nomOuvrage,idOuvrage}",function(r) {
+            for (var i=0;i<r.data.length;i++) {
+                TMap.setMarker(r.data[i].oa_y,r.data[i].oa_x,r.data[i].nomOuvrage,r.data[i].idOuvrage);
+            }
+        });*/  
+            
+            
+            
+            
+         
         
-            
-            
-            
-            
-            
+        
+        
+        
             
             
   /*           
