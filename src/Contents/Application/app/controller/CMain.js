276985c85baaff5ec0cb64a2dcc6400b30aea139
bobotoms <bobotoms@yahoo.fr>
2017-04-06 14:44:41 +0200

    Update# 642

--- src/Contents/Application/app/controller/CMain.js
+++ src/Contents/Application/app/controller/CMain.js
@@ -177,6 +177,9 @@ App.controller.define('CMain', {
                 itemdblclick: "add_visit_select"
             },
             "VAddVisit":{
+                show: "showMapAddV"
+            },
+            "VVisit":{
                 show: "showMapV"
             },
             "VVisitWork": {
@@ -1220,9 +1223,16 @@ App.controller.define('CMain', {
         form.idOuvrage=store.data.idOuvrage;
         form.show();
     },
-    showMapV: function() {
+    showMapAddV: function() {
                     new google.maps.Map(document.getElementById('TMapPanel2'),{
-                        zoom: 8,
+                        zoom: 10,
+                        center: new google.maps.LatLng('43.299999','5.4'),
+                        mapTypeId: google.maps.MapTypeId.MAP
+        });
+    },
+    showMapV: function() {
+                    new google.maps.Map(document.getElementById('TMapPanel3'),{
+                        zoom: 10,
                         center: new google.maps.LatLng('43.299999','5.4'),
                         mapTypeId: google.maps.MapTypeId.MAP
         });
