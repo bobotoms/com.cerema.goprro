0458aeb46f86860103153b7a3a9d497a46732e9b
bobotoms <bobotoms@yahoo.fr>
2017-04-06 16:22:08 +0200

    Update# 690

--- src/Contents/Application/app/controller/CMain.js
+++ src/Contents/Application/app/controller/CMain.js
@@ -1035,7 +1035,40 @@ App.controller.define('CMain', {
         //hideForms();
         App.get("mainform panel#visit").show();
         var mail = Auth.User.mail;
-        //TMap.clearMarkers();
+        TMap.clearMarkers();
+        
+        App.Visits.select(mail,function(response) {
+            
+        console.log("response");
+        console.log(response);
+            var data=[];
+            for (var i=0;i<response.length;i++) {
+                data.push({
+                    idOuvrage:response[i].idOuvrage,
+                    nomOuvrage:response[i].nomOuvrage,
+                    nomDepartement:response[i].nomDepartement,
+                    oa_x:response[i].oa_x,
+                    oa_y:response[i].oa_y
+                })
+                TMap.setMarker(response[i].oa_y,response[i].oa_x,response[i].nomOuvrage,response[i].idOuvrage);
+            };
+            var store=App.store.create({
+                fields:["idOuvrage","nomOuvrage","nomDepartement","oa_x","oa_y"],data:data
+            });
+            if(store)
+            {
+                App.get('VAddVisit grid#gridVisitAdd').bindStore(store);
+                store.load();
+            }
+            
+        App.get("VAddVisit panel#addVisitMap").show();
+        App.get("VAddVisit panel#map").show();
+
+        });
+        
+        
+        
+        
         var choixDate = App.get('VVisit combo#dateVisit').getValue();
         var tabDate = [mail, choixDate];
         App.Visits.selectVisitDate(tabDate,function(response) {
