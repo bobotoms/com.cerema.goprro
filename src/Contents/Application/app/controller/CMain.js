22a0c187e09cb1c925077a292ef5a54598e2bc8f
bobotoms <bobotoms@yahoo.fr>
2017-04-06 15:01:55 +0200

    Update# 651

--- src/Contents/Application/app/controller/CMain.js
+++ src/Contents/Application/app/controller/CMain.js
@@ -958,7 +958,7 @@ App.controller.define('CMain', {
         
         
         TMap.clearMarkers();
-        App.get("VAddVisit panel#map").show();
+        App.get("VAddVisit panel#addVisitMap").show();
         
         App.Visits.select(mail,function(response) {
             
