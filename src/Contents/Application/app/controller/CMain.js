2b624a1d93b28306f7ee8410fa4fa54f1a18cba9
bobotoms <bobotoms@yahoo.fr>
2017-04-07 08:34:15 +0200

    Update# 719

--- src/Contents/Application/app/controller/CMain.js
+++ src/Contents/Application/app/controller/CMain.js
@@ -1034,7 +1034,6 @@ App.controller.define('CMain', {
         });
     },
     showVisitDate: function(p) {
-    
         
         App.get('mainform panel#southpanel').collapse();
         //hideForms();
@@ -1044,8 +1043,8 @@ App.controller.define('CMain', {
         
         App.Visits.select(mail,function(response) {
             
-        console.log("response");
-        console.log(response);
+            console.log("response");
+            console.log(response);
             var data=[];
             for (var i=0;i<response.length;i++) {
                 data.push({
@@ -1059,33 +1058,32 @@ App.controller.define('CMain', {
                 TMap.setMarker(response[i].oa_y,response[i].oa_x,response[i].nomOuvrage,response[i].idOuvrage);
             };
             var choixDate = App.get('VVisit combo#dateVisit').getValue();
-        var tabDate = [mail, choixDate];
-        App.Visits.selectVisitDate(tabDate,function(response) {
-            var data=[];
-            for (var i=0;i<response.length;i++) {
-                data.push({
-                    idVisiteOuvrage:response[i].idVisiteOuvrage,
-                    nomOuvrage:response[i].nomOuvrage,
-                    nomDepartement:response[i].nomDepartement,
-                    oa_x:response[i].oa_x,
-                    oa_y:response[i].oa_y
-                })
-                
-                TMap.setMarker(response[i].oa_y,response[i].oa_x,response[i].nomOuvrage,response[i].idOuvrage,"jaune");
-            };
-            var store=App.store.create({
-                fields:["idVisiteOuvrage","nomOuvrage","nomDepartement","oa_x","oa_y"],data:data
-            });
-            if(store)
-            {
-                App.get('VVisit grid#gridVisit').bindStore(store);
-                store.load();
-            }
-            App.get('VVisit grid').show();
-            
-        });
-           
+            var tabDate = [mail, choixDate];
+            App.Visits.selectVisitDate(tabDate,function(response) {
+                var data=[];
+                for (var i=0;i<response.length;i++) {
+                    data.push({
+                        idVisiteOuvrage:response[i].idVisiteOuvrage,
+                        nomOuvrage:response[i].nomOuvrage,
+                        nomDepartement:response[i].nomDepartement,
+                        oa_x:response[i].oa_x,
+                        oa_y:response[i].oa_y
+                    })
 
+                    TMap.setMarker(response[i].oa_y,response[i].oa_x,response[i].nomOuvrage,response[i].idOuvrage,"jaune");
+                };
+                var store=App.store.create({
+                    fields:["idVisiteOuvrage","nomOuvrage","nomDepartement","oa_x","oa_y"],data:data
+                });
+                if(store)
+                {
+                    App.get('VVisit grid#gridVisit').bindStore(store);
+                    store.load();
+                }
+                App.get('VVisit grid').show();
+
+            });
+           
         });
         
         
