35033b2475f1688d5d560f90031b2db23c20d93f
bobotoms <bobotoms@yahoo.fr>
2017-04-10 16:23:35 +0200

    Update# 1010

--- src/Contents/Application/app/controller/CMain.js
+++ src/Contents/Application/app/controller/CMain.js
@@ -1349,7 +1349,7 @@ App.controller.define('CMain', {
         var mail = Auth.User.mail;
         App.get('VAddVisitWork').close();
         TMap.clearMarkers();
-         var store=App.get("VAddVisit grid#add_visit").getStore().data;
+         var store=App.get("VAddVisit grid#add_visit");
         console.log("store");
         console.log(store);
         /******************   regarder le nomnbre de ligne parcourir chaque ligne(items) si c'est selected :
