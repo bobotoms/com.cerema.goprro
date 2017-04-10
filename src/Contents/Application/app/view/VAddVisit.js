b0dc54bd1b9fd0e9db49c7a799ca7fbbacb9835c
bobotoms <bobotoms@yahoo.fr>
2017-04-10 11:21:03 +0200

    Update# 894

--- src/Contents/Application/app/view/VAddVisit.js
+++ src/Contents/Application/app/view/VAddVisit.js
@@ -81,6 +81,27 @@ App.view.define('VAddVisit', {
                     xtype: "checkbox",
                     itemId: "select",
                     cls: 'x-grid-checkheader-editor'
+                },
+                singleSelect: true,
+                listeners: {
+                    click: function(sm,index,record) {
+                        var idVisiteOuvrage = sm.store.data.items[record].data.idVisiteOuvrage;
+                        console.log("modif id viste ouvrage");
+                        console.log(idVisiteOuvrage);
+                        /*ici, tu mets tous tes traitements lors d'un clic sur une ligne*/
+                        /*je t ai mis une alerte ... */
+                        /*pour utiliser un parametre de ta ligne, tu utilises record.data.[nom de la colonne]*/
+                        
+                    },
+                    check: function(sm,index,record) {
+                        var idVisiteOuvrage = sm.store.data.items[record].data.idVisiteOuvrage;
+                        console.log("modif id viste ouvrage");
+                        console.log(idVisiteOuvrage);
+                        /*ici, tu mets tous tes traitements lors d'un clic sur une ligne*/
+                        /*je t ai mis une alerte ... */
+                        /*pour utiliser un parametre de ta ligne, tu utilises record.data.[nom de la colonne]*/
+                        
+                    }
                 }
             }],
         }],
