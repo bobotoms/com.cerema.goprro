411520e568a782120050eeaab5fe790d3015a027
bobotoms <bobotoms@yahoo.fr>
2017-04-10 09:35:31 +0200

    Update# 843

--- src/Contents/Application/app/controller/CMain.js
+++ src/Contents/Application/app/controller/CMain.js
@@ -1286,6 +1286,7 @@ App.controller.define('CMain', {
  /*       App.get("VVisitWork window#VisiteWork").show();
         var form = App.get("VVisitWork window#VisiteWork");*/
         
+        App.get("mainform window#Work").show();
         console.log("form");
         console.log(form);
         form.idOuvrage=store.data.idOuvrage;
