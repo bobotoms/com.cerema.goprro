16b12a808869612d368eed29277e48a774bed2c3
bobotoms <bobotoms@yahoo.fr>
2017-04-10 16:25:01 +0200

    Update# 1011

--- src/Contents/Application/app/controller/CMain.js
+++ src/Contents/Application/app/controller/CMain.js
@@ -1349,6 +1349,7 @@ App.controller.define('CMain', {
         var mail = Auth.User.mail;
         App.get('VAddVisitWork').close();
         TMap.clearMarkers();
+         var store=me.up('panel');
          var store=App.get("VAddVisit grid#add_visit");
         console.log("store");
         console.log(store);
