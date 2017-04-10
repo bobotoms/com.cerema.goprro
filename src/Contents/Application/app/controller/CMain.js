bcfe6e8376abe9e56f12852688c5d8523b133be9
bobotoms <bobotoms@yahoo.fr>
2017-04-10 10:00:26 +0200

    Update# 856

--- src/Contents/Application/app/controller/CMain.js
+++ src/Contents/Application/app/controller/CMain.js
@@ -1276,12 +1276,11 @@ App.controller.define('CMain', {
         App.get('mainform panel#southpanel').collapse();
         //var form=App.get("mainform window#Work");
         //var form=App.get("mainform panel#VisiteWork");
- //       var form=App.get("mainform panel#Work");
-  //      var form=App.get("mainform panel#Work");
+        var form=App.get("mainform panel#Work");
         //var form=me;
         //var form=App.get("VVisitWork window#Work");
  //       var form = App.get("VVisitWork window#VisiteWork");
-        var form = App.get("VVisitWork window#VisiteWork2");
+        //var form = App.get("VVisitWork window#VisiteWork2");
         //var form = App.get("VVisitWork");
         
         
