c870b1303a361306e88cf71e068ece0ccd29d092
bobotoms <bobotoms@yahoo.fr>
2017-04-11 08:10:06 +0200

    Update# 1024

--- src/Contents/Application/app/controller/CMain.js
+++ src/Contents/Application/app/controller/CMain.js
@@ -1364,14 +1364,14 @@ App.controller.define('CMain', {
          //var store=App.get("VAddVisit grid#add_visit");
         console.log("store");
         console.log(store);
-        
+        var items = store.data.items;
 
         
             var datas=[];
-            for (var i=0;i<store.data.items.length;i++) 
+            for (var i=0;i<items.length;i++) 
             {
                  App.Visits.select(mail,function(response) {
-                 if(store.data.items[i].select == true)
+                 if(items[i].select == true)
                  {
                      datas.push({
                         idOuvrage:response[i].idOuvrage,
