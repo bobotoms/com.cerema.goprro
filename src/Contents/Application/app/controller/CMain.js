884bf534738961bfdba6170076eb95edd0b69f64
bobotoms <bobotoms@yahoo.fr>
2017-04-11 11:10:02 +0200

    Update# 1089

--- src/Contents/Application/app/controller/CMain.js
+++ src/Contents/Application/app/controller/CMain.js
@@ -1180,7 +1180,7 @@ App.controller.define('CMain', {
                     oa_x:response[i].oa_x,
                     oa_y:response[i].oa_y
                 })
-                TMap.setMarker(response[i].oa_y,response[i].oa_x,response[i].nomOuvrage,response[i].idOuvrage"","visit");
+                TMap.setMarker(response[i].oa_y,response[i].oa_x,response[i].nomOuvrage,response[i].idOuvrage,"","visit");
             };
         });        
         App.Visits.selectVisit(mail,function(response) {
