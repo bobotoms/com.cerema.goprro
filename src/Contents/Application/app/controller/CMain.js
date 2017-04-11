13eb93facc304dd3c70272cfbbb82bd49a31e05a
bobotoms <bobotoms@yahoo.fr>
2017-04-11 15:42:57 +0200

    Update# 1139

--- src/Contents/Application/app/controller/CMain.js
+++ src/Contents/Application/app/controller/CMain.js
@@ -1563,7 +1563,7 @@ App.controller.define('CMain', {
              console.log(paneltest2.items.items[3].items.items[0].items.items[3].value); //        text gestionnaire
              console.log(paneltest2.items.items[3].items.items[0].items.items[4].value);//         id poseur
              console.log(paneltest2.items.items[3].items.items[0].items.items[5].value);//         text gestionnaire
-             console.log(paneltest2.items.items[3].items.items[1].items.items[0].value);//         Mat necess
+             console.log(paneltest2.items.items[3].items.items[1]);//         Mat necess
         
         
             var paneltest = App.get(me.up('panel'),"grid");
