a40009340b394e24ce381819755f2e923fabdc9e
bobotoms <bobotoms@yahoo.fr>
2017-04-13 16:14:52 +0200

    Update# 1304

--- src/Contents/Services/Visits.js
+++ src/Contents/Services/Visits.js
@@ -1,6 +1,9 @@
 Visits = {
     select: function(o,cb) {
- 		Visits.using('db').query("goprro","SELECT * FROM ouvrages left join familles on ouvrages.idFamille=familles.idFamille left join types on types.idType=ouvrages.idType left join geologies on geologies.idGeologie=ouvrages.idGeologie left join situations on situations.idSituation=ouvrages.idSituation left join acces on acces.idAcces=ouvrages.idAcces left join departements on departements.idDepartement=ouvrages.idDepartement WHERE departements.idDter = (SELECT idDter FROM users WHERE mail='"+o+"')",function(err,result){
+        
+        var mail = o['0'];
+        var idCampagne = o['1'];
+ 		Visits.using('db').query("goprro","SELECT * FROM ouvrages left join familles on ouvrages.idFamille=familles.idFamille left join types on types.idType=ouvrages.idType left join geologies on geologies.idGeologie=ouvrages.idGeologie left join situations on situations.idSituation=ouvrages.idSituation left join acces on acces.idAcces=ouvrages.idAcces left join departements on departements.idDepartement=ouvrages.idDepartement WHERE idCampagne = "+idCampagne+" departements.idDter = (SELECT idDter FROM users WHERE mail='"+mail+"')",function(err,result){
             if (!err) {
                     console.log("result");
                     console.log(result);
