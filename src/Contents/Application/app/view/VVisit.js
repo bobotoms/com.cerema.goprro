bd96553dafe89d2217a76af58deeeb3050f74c19
bobotoms <bobotoms@yahoo.fr>
2017-04-14 11:45:19 +0200

    Update# 1430

--- src/Contents/Application/app/view/VVisit.js
+++ src/Contents/Application/app/view/VVisit.js
@@ -45,7 +45,7 @@ App.view.define('VVisit', {
             valueField: "idCampagne",
             editable: false,
             renderer: Ext.util.Format.dateRenderer('mm/dd/yy'),
-            store: App.store.create("goprro://campagne",{autoLoad:true}),
+            store: App.store.create("goprro://campagne{dateDebut,dateFin}",{autoLoad:true}),
      /*       renderer: function(value,a,b,c){
                 Ext.Msg.alert('GOPRRO',"test renderer");
                 console.log("value");
