a256d0ac91f27f90ef5e5d0f65c8033bcc560e4c
bobotoms <bobotoms@yahoo.fr>
2017-04-18 10:00:21 +0200

    Update# 1490

--- src/Contents/Application/app/view/VVisit.js
+++ src/Contents/Application/app/view/VVisit.js
@@ -40,6 +40,7 @@ App.view.define('VVisit', {
             itemId: "idCampagne",
             fieldLabel: "Période de visite",
             padding: 5,
+                width: 150,
             //displayField: "dateDebut",
             displayField: "periode",
             //displayField: "idCampagne",
