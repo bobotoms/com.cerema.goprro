f5f5ef6f7b617aac999dec475f5bbd2769686414
bobotoms <bobotoms@yahoo.fr>
2017-04-18 08:02:45 +0200

    Update# 1457

--- src/Contents/Application/app/controller/CMain.js
+++ src/Contents/Application/app/controller/CMain.js
@@ -191,6 +191,8 @@ function GMap(l,m)
 
    // App.DB.get("goprro://ouvrages{idOuvrage,oa_x,oa_y,nomOuvrage,idOuvrage}",function(r) {
     App.DB.get("goprro://ouvrages{oa_x,oa_y,nomOuvrage,idOuvrage}",function(r) {
+        console.log("r map");
+        console.log(r);
         for (var i=0;i<r.data.length;i++) {
             TMap.setMarker(r.data[i].oa_y,r.data[i].oa_x,r.data[i].nomOuvrage,r.data[i].idOuvrage);
         }
