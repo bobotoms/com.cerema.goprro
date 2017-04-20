652adae41cc1a058e5bda80a199f096e40f60cf6
bobotoms <bobotoms@yahoo.fr>
2017-04-20 09:53:59 +0200

    Update# 1719

--- src/Contents/Application/app/controller/CMain.js
+++ src/Contents/Application/app/controller/CMain.js
@@ -1715,10 +1715,11 @@ App.controller.define('CMain', {
   /*              console.log("rrrrrrrr");
                 console.log(r);*/
                 //if (re.data[0]._BLOB) App.get(me,'uploadfilemanager#up').setFiles(JSON.parse(re.data[0]._BLOB));
+                if (App.get('VUpVisitWork uploadfilemanager#up').getFiles()) App.get('VUpVisitWork uploadfilemanager#up').setFiles(JSON.parse(re.data[0]._BLOB));
                 // On post l'upload
-                App.Docs.upload(App.get('uploadfilemanager#up').getFiles(),0,function() {
+         /*       App.Docs.upload(App.get('uploadfilemanager#up').getFiles(),0,function() {
                     //alert('posté!');
-                });
+                });*/
 /*
                 console.log("me.up('panel') visit");
                 console.log(me.up('panel'));
