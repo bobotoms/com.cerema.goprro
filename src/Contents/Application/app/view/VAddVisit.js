6a93932cec2df12ca1234cb289a7aa680916ddd9
bobotoms <bobotoms@yahoo.fr>
2017-04-21 08:54:03 +0200

    Update# 1788

--- src/Contents/Application/app/view/VAddVisit.js
+++ src/Contents/Application/app/view/VAddVisit.js
@@ -111,9 +111,11 @@ App.view.define('VAddVisit', {
                             console.log("App.get(sm.up('panel'),\"grid\")");
                             console.log(sm.up('panel'),"grid");
                             var grid = App.get(sm.up('panel'),"grid");
+                            console.log("grid");
+                            console.log(grid);
                             //sm.up('panel'),"grid".items.items[0].dataSource.data.items[index].data;
-                            console.log("sm.up('panel'),\"grid\".items.items[0].dataSource.data.items[index].data");
-                            console.log(grid.items.items[0].dataSource.data.items[index].data);
+                 /*           console.log("sm.up('panel'),\"grid\".items.items[0].dataSource.data.items[index].data");
+                            console.log(grid.items.items[0].dataSource.data.items[index].data);*/
                             
                             console.log("sm.up('panel')");
                             console.log(sm.up('panel'));
