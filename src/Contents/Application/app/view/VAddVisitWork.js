5b5e821abe1e5e0f47fec54704dc54e1148e6c9d
bobotoms <bobotoms@yahoo.fr>
2017-04-24 10:41:20 +0200

    Update# 1860

--- src/Contents/Application/app/view/VAddVisitWork.js
+++ src/Contents/Application/app/view/VAddVisitWork.js
@@ -48,7 +48,7 @@ App.view.define('VAddVisitWork', {
                     bindTo: "oa_lambert_proj",
 					itemId: "oa_lambert_proj",
 					editable: false,
-					store: App.store.create('goprro://projections'),
+					store: App.store.create('goprro://projections',{autoLoad:true}),
 					displayField: "projection_name",
 					valueField: "idprojections",
                     width: "100%",
