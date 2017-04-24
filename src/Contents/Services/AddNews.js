1cbbfd45228192d7590d9bc2430d2d4ba8b2dec6
bobotoms <bobotoms@yahoo.fr>
2017-04-24 10:44:42 +0200

    Update# 1864

--- src/Contents/Services/AddNews.js
+++ src/Contents/Services/AddNews.js
@@ -5,25 +5,12 @@ AddNews = {
         var diffusion = o['1'];
         var note = o['2'];
         var importance = o['3'];
-
-        console.log("idUser");
-        console.log(idUser);
-        console.log("note");
-        console.log(note);
-        console.log("dif");
-        console.log(diffusion);
-        console.log("importance");
-        console.log(importance);
         
         AddNews.using('db').query("goprro","INSERT INTO notes (dateNote, diffusion, texteNote, importance, idUser) VALUES (NOW(), "+diffusion+", '"+note+"', '"+importance+"', "+idUser+")",function(err,result){
             if (!err) {
-                console.log("result");
-                console.log(result);
                 var response=true;
                 cb(response);
             } else {
-                console.log("err");
-                console.log(err);
                 var err=false;
                 cb(err)
             };
